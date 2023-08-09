cdbcb5b333c80d7cae750d73a1837da68bb33db0
weforever <117436754+weforever@users.noreply.github.com>
2023-08-09 12:03:35 +0800

    Update data.py

--- protonet_score/data.py
+++ protonet_score/data.py
@@ -2,16 +2,13 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 from sklearn.model_selection import train_test_split
-# 读取和预处理数据
+# Reading and pre-processing data
 def load_data():
-  data_u = pd.read_csv(r'D:/protonet_1/data_zhinengche.csv')
-  data_use = data_u.loc[:,['车速', '累计里程', '总电压', '总电流', 'SOC', 'DC-DC状态', '挡位', '挡位驱动力', '挡位制动力',
-            '最高电压电池单体代号', '电池单体电压最高值', '最低电压电池单体代号', '电池单体电压最低值',
-            '最高温度探针单体代号', '最高温度值', '最低温度探针子系统代号', '最低温度值',
-            '可充电储能装置故障总数']]
+  data_u = pd.read_csv(r'your file')
+  data_use = data_u.loc[:,['Enter your data characteristics']]
 
   X = data_use.copy()
-  y = X.pop('可充电储能装置故障总数')
+  y = X.pop('target')
 
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
 
@@ -24,5 +21,5 @@ def load_data():
   X_train_scaled = scaler.fit_transform(X_train)
   X_test_scaled = scaler.fit_transform(X_test)
 
-  # 返回预处理后的数据
+  # Returns preprocessed data
   return X_train_scaled, y_train, X_test_scaled, y_test
